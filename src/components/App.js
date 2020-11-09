282d8128bea41fd37fe79a60e2bf7211f148e8f8
VanGamo <vangamo.beta@gmail.com>
2020-11-09 17:46:52 +0100

    Add Result component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./App.scss";
 import Header from "./Header";
+import ResultJson from "./ResultJson";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.result= {text:"Testing result"};
+  }
+
   render() {
     return (
       <div className="App">
         <Header number="2.6" lesson="Estado" title="Formulario" subtitle="Con JSON!" />
         <main className="container">
+          <ResultJson result={this.result} />
         </main>
       </div>
     );
---
+++ src/components/ResultJson.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import './ResultJson.scss';
+
+class ResultJson extends React.Component {
+  render() {
+    console.log(this.props);
+    
+    const jsonText = JSON.stringify(this.props.result);
+
+    const jsonPrettyText = jsonText;
+
+    return (
+      <section className="result">
+        <h3 className="result__title">JSON resultante:</h3>
+        <pre className="result__json">{jsonPrettyText}</pre>
+      </section>
+    );
+  }
+}
+
+ResultJson.propTypes = {
+  result: PropTypes.object.isRequired
+};
+
+export default ResultJson;
